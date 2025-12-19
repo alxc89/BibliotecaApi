757463a082bda02d3948ee7a36d1dd329a2cf6ca
alxc89 <alexcaardoso@gmail.com>
2025-12-18 23:20:02 -0300

    [feat] novos endpoints para cadastrar com senha e login

--- Application/Api/Controllers/UsuarioController.cs
+++ Application/Api/Controllers/UsuarioController.cs
@@ -3,6 +3,7 @@ using BibliotecaApi.UseCases.Usuario.DTO;
 using BibliotecaApi.Application.Api.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BibliotecaApi.Application.Api.Controllers;
 
@@ -11,12 +12,14 @@ namespace BibliotecaApi.Application.Api.Controllers;
 public class UsuarioController : Controller
 {
     private readonly CadastrarUsuarioUC _cadastrarUsuarioUC = new CadastrarUsuarioUC();
-
+    private readonly LoginUsuarioUC _loginUsuarioUC = new LoginUsuarioUC();
+    
     [HttpPost]
     [SwaggerOperation(Summary = "Cadastra um novo usuário retornando o seu respectivo Id")]
     [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ApiResponse<int>))]
     [SwaggerResponse(StatusCodes.Status400BadRequest)]
     [SwaggerResponse(StatusCodes.Status401Unauthorized)]
+    [AllowAnonymous]
     public async Task<IActionResult> Cadastrar([FromBody] CadastrarUsuarioInputDTO input)
     {
         try
@@ -26,6 +29,27 @@ public class UsuarioController : Controller
                 return BadRequest(ApiResponse<int?>.Falha(response.MensagemErro!));
             
             return Ok(ApiResponse<int>.Ok(response.Dados));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ApiResponse<int>.Falha("Erro ao cadastrar usuário: " + ex.Message));
+        }
+    }
+
+    [HttpPost]
+    [SwaggerOperation(Summary = "Efetua o login um novo token de autenticação")]
+    [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ApiResponse<int>))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest)]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login([FromBody] LoginUsuarioInput input)
+    {
+        try
+        {
+            var response = await _loginUsuarioUC.Execute(input);
+            if (!response.Sucesso)
+                return BadRequest(ApiResponse<int?>.Falha(response.MensagemErro!));
+            
+            return Ok(ApiResponse<TokenDTO>.Ok(response.Dados));
         }
         catch (Exception ex)
         {
